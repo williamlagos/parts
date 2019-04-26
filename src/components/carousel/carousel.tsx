b07b222d4a1a58a15b5701dbbcd33cb1038308a6
William Oliveira de Lagos <william.lagos@icloud.com>
2019-04-26 03:44:00 -0300

    Hotfix for SVG scaling on tour

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -50,7 +50,9 @@ export class Carousel {
         <ion-slides pager={false}>
 
           <ion-slide>
-            <img src="assets/img/tour_1_clipper.svg" class="slide-image"/>
+            <div class="slide-image-container">
+              <img src="assets/img/tour_1_clipper.svg" class="slide-image"/>
+            </div>
             <h2 class="slide-title">
               Bem-vindo ao <b>Frete Fácil</b>
             </h2>
@@ -64,7 +66,9 @@ export class Carousel {
           </ion-slide>
 
           <ion-slide>
-            <img src="assets/img/tour_2_map.svg" class="slide-image"/>
+            <div class="slide-image-container">
+              <img src="assets/img/tour_2_map.svg" class="slide-image"/>
+            </div>
             <h2 class="slide-title">Por que usar o Frete Fácil?</h2>
             <p>
               <b>Frete Fácil</b> conta com prestadores de entrega e mudança bem selecionados e revisados pela nossa equipe.</p>
@@ -75,7 +79,9 @@ export class Carousel {
           </ion-slide>
 
           <ion-slide>
-            <img src="assets/img/tour_3_payment.svg" class="slide-image"/>
+            <div class="slide-image-container">
+              <img src="assets/img/tour_3_payment.svg" class="slide-image"/>
+            </div>
             <h2 class="slide-title">Pago a mais para usar este serviço?</h2>
             <p>
               O aplicativo é <b>100% gratuito</b>, apenas é cobrado o valor que é combinado pela plataforma entre o freteiro e você.</p>
@@ -86,7 +92,9 @@ export class Carousel {
           </ion-slide>
 
           <ion-slide>
-            <img src="assets/img/tour_4_start.svg" class="slide-image"/>
+            <div class="slide-image-container">
+              <img src="assets/img/tour_4_start.svg" class="slide-image"/>
+            </div>
             <h2 class="slide-title">Pronto para Começar?</h2>
             <ion-button fill="clear" href="#schedule" onClick={(e) => this.finishTutorial(e)}>
               Continuar
